46df762e85e35054410d385009e14b488a0134a3
Anurag <anuragtiw112@gmail.com>
2023-03-04 16:10:46 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+
+// to use req.body
+app.use(express.json());
+app.use(cors());
+
+app.use("/", require("./routes/public.js"));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is listening at port 5000");
+});
