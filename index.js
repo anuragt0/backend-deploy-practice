c31e0c4c3860e511328ac95304567dfffae27489
Anurag <anuragtiw112@gmail.com>
2023-03-05 19:44:54 +0530

    temp

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(cors());
 app.use("/", require("./routes/public.js"));
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is listening at port 5000");
+  console.log("Server is listening at port 5000.");
 });
