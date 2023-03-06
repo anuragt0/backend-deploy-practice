32da28b19cbf6d1fd2f5674bf69044e49c6251dc
Anurag <anuragtiw112@gmail.com>
2023-03-06 22:31:29 +0530

    added some more code

--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.json({"success": true})
+    res.json({"success": true, "another": "Hey whatsup"})
 })
 
 
