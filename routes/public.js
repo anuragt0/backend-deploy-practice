a34521a9366ebd863f272bd5589ba9459b03bd31
Anurag <anuragtiw112@gmail.com>
2023-03-07 22:20:08 +0530

    Old working code again

---
+++ routes/public.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const User = require('../db/Models/User');
+
+router.get('/', (req,res)=>{
+    res.json({"success": true, "another": "Hey whatsup"})
+})
+router.post('/post', (req,res)=>{
+
+    const data = req.body;
+    res.send(data);
+})
+
+router.get('/getdata', async (req,res)=>{
+    const allUsers = await User.find();
+    res.json({"success: ": true, "data": allUsers});
+})
+
+
+module.exports = router;
