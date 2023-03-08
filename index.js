091fa09a86076786ff417cc281cf5211bace035e
Anurag <anuragtiw112@gmail.com>
2023-03-08 07:10:47 +0530

    added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 // app.use("/", require("./src/api/routes/public"));
 app.use("/api/auth/user", require("./src/api/routes/user"));
+app.use("/api/auth/admin", require("./src/api/routes/admin"));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is listening at port 5000.");
