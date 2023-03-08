ba118fdda76bac9bb3cd5afd46698bd1c2c12d3a
Anurag <anuragtiw112@gmail.com>
2023-03-08 12:06:28 +0530

    asdf

--- src/databases/firebase/config.js
+++ src/databases/firebase/config.js
@@ -1,11 +1,11 @@
-// require("dotenv").config();
-// const admin = require("firebase-admin");
-// const { getStorage } = require("firebase-admin/storage");
+require("dotenv").config();
+const admin = require("firebase-admin");
+const { getStorage } = require("firebase-admin/storage");
 
-// // todo: can we init firebase once like mongodb
+// todo: can we init firebase once like mongodb
 
-// const serviceAccount = require("./service_account");
-// // console.log(serviceAccount);
+const serviceAccount = require("./service_account");
+// console.log(serviceAccount);
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
